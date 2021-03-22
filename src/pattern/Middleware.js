6a39e342c33b64124060b5feac20371339764b17
mertcan <mertcanyedek2@gmail.com>
2021-03-22 21:35:50 +0300

    Push of null exception is solved

--- src/pattern/Middleware.js
+++ src/pattern/Middleware.js
@@ -1,7 +1,7 @@
 class Middleware {
     constructor(newDao,gui) {
         this._dao = newDao;
-        this._objects = []
+        this._objects =localStorage.getItem("objects")? JSON.parse(localStorage.getItem("objects")):[];
         this._gui = gui;
     }
     getDAO() {
