271338f568c1cf9a0e865cac17a481be31cd7c40
b21627428 <b21627428@cs.hacettepe.edu.tr>
2021-03-29 22:18:03 +0300

    preview dinamik hale getirildi.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,19 +1,26 @@
 import { AppstoreAddOutlined } from "@ant-design/icons";
 import ContextConsumer from "../contextapi/Context";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import obj2xml from "../helper/obj2xml";
+
+import XMLViewer from 'react-xml-viewer'
+import ReactJson from 'react-json-view'
+
 
 export default function Preview() {
   const { collections } = useSelector((state) => state.collectionReducer);
   return (
     <ContextConsumer>
       {(value) => {
-        const { document, collection } = value;
+        const { document, collection, isJSON } = value;
         let currDocument;
-        if(collection !== undefined && document !== undefined){
-            currDocument = collections.filter((c) => {
-                return c.id === collection.id;
-            })[0].documents.filter(d => {
-                return d.id === document.id;
+        if (collection !== undefined && document !== undefined) {
+          currDocument = collections
+            .filter((c) => {
+              return c.id === collection.id;
+            })[0]
+            .documents.filter((d) => {
+              return d.id === document.id;
             })[0];
         }
         return (
@@ -24,8 +31,12 @@ export default function Preview() {
                 <AppstoreAddOutlined className="d-flex mt-1 justify-content-end" />
               </div>
             </div>
-            <div>
-                {JSON.stringify(currDocument)}
+            <div className="p-3 ">
+              {currDocument
+                ? isJSON
+                  ? <ReactJson src={currDocument} />
+                  : <XMLViewer xml={obj2xml(currDocument)} />
+                : null}
             </div>
           </div>
         );
