29f419443cdc9085a751958bfc0c29e6bf006e4d
mertcan <mertcanyedek2@gmail.com>
2021-04-02 22:33:39 +0300

    Content panel is initialized

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,16 +3,19 @@ import ContextConsumer from "../contextapi/Context";
 import { useSelector } from "react-redux";
 import obj2xml from "../helper/obj2xml";
 
-import XMLViewer from 'react-xml-viewer'
-import ReactJson from 'react-json-view'
-
+import XMLViewer from "react-xml-viewer";
+import ReactJson from "react-json-view";
+import { useState } from "react";
+import { JsonEditor } from "react-json-edit";
 
 export default function Preview() {
   const { collections } = useSelector((state) => state.collectionReducer);
   const customTheme = {
-    "attributeKeyColor": "#FF0000",
-    "attributeValueColor": "#461634"
-  }
+    attributeKeyColor: "#FF0000",
+    attributeValueColor: "#461634",
+  };
+  const isModalVisible = useState(false);
+
   return (
     <ContextConsumer>
       {(value) => {
@@ -23,7 +26,7 @@ export default function Preview() {
             .filter((c) => {
               return c.id === collection.id;
             })[0]
-            .documents.filter((d) => {
+            ?.documents.filter((d) => {
               return d.id === document.id;
             })[0];
         }
@@ -32,15 +35,31 @@ export default function Preview() {
             <div className="p-3 bg-light border border-1">
               <div className="d-flex justify-content-between">
                 Content
-                <AppstoreAddOutlined className="d-flex mt-1 justify-content-end" />
+                <AppstoreAddOutlined
+                  className="d-flex mt-1 justify-content-end"
+                  //onClick={() => updateContent()}
+                />
               </div>
             </div>
             <div className="p-3 ">
-              {currDocument
-                ? isJSON
-                  ? <ReactJson src={currDocument} />
-                  : <XMLViewer xml={obj2xml(currDocument)} theme={customTheme} indentSize={6} collapsible={true} />
-                : null}
+              {currDocument ? (
+                isJSON ? (
+                  <JsonEditor
+                    className="bg-light"
+                    style={{ border: "none" }}
+                    tableLike={true}
+                    value={currDocument}
+                  />
+                ) : (
+                  //<ReactJson src={currDocument} />
+                  <XMLViewer
+                    xml={obj2xml(currDocument)}
+                    theme={customTheme}
+                    indentSize={6}
+                    collapsible={true}
+                  />
+                )
+              ) : null}
             </div>
           </div>
         );
