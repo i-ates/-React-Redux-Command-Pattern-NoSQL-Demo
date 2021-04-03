c37a73a95d40c81df2ccf5791b294705ca685f2f
b21627428 <b21627428@cs.hacettepe.edu.tr>
2021-04-03 14:20:34 +0300

    test environment created.

---
+++ src/pattern/tests/Trigger.test.js
@@ -0,0 +1,98 @@
+import Trigger from '../Trigger'
+
+// .env içinde SKIP_PREFLIGHT_CHECK=true olması lazım.
+
+let dummyData = {id:1, name:"collection1",documents:[{
+    id:1, name:"document1", content:{
+        name:"said",
+        age:"23"
+    }
+}]};
+
+test("Add should work fine",()=> {
+    const trigger= new Trigger();
+
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+} );
+
+test("Remove should work fine", ()=> {
+    const trigger = new Trigger();
+    
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    trigger.removeCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(0);
+});
+
+test("Update should work fine", ()=> {
+    const trigger = new Trigger();
+    
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    
+    let updatedData= {...dummyData};
+    updatedData.documents= [...dummyData.documents];
+    updatedData.documents.push({
+        id:2, name:"document2", content:{
+            name:"ismail",
+            age:"24"
+        }
+    });
+
+    trigger.updateCollection(dummyData,updatedData);
+    expect(trigger.getInvoker().getMiddleware().getCollections()[0].documents.length).toBe(2);
+});
+
+test("Undo should work fine",()=> {
+    const trigger= new Trigger();
+
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    expect(trigger.getInvoker().getUndoCommands().length).toBe(1);
+    trigger.undo();
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(0);
+    expect(trigger.getInvoker().getUndoCommands().length).toBe(0);
+});
+
+test("Redo should work fine",()=> {
+    const trigger= new Trigger();
+
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    expect(trigger.getInvoker().getUndoCommands().length).toBe(1);
+    trigger.undo();
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(0);
+    expect(trigger.getInvoker().getUndoCommands().length).toBe(0);
+    expect(trigger.getInvoker().getRedoCommands().length).toBe(1);
+
+    trigger.redo();
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    expect(trigger.getInvoker().getRedoCommands().length).toBe(0);
+});
+
+test("Commit should work fine", ()=>{
+    const trigger = new Trigger();
+    
+    trigger.addCollection(dummyData);
+    expect(trigger.getInvoker().getMiddleware().getCollections().length).toBe(1);
+    
+    let updatedData= {...dummyData};
+    updatedData.documents= [...dummyData.documents];
+    updatedData.documents.push({
+        id:2, name:"document2", content:{
+            name:"ismail",
+            age:"24"
+        }
+    });+
+
+    trigger.updateCollection(dummyData,updatedData);
+    expect(trigger.getInvoker().getMiddleware().getCollections()[0].documents.length).toBe(2);
+
+    trigger.commit();
+    expect(trigger.getInvoker().getMiddleware().getCollections()[0].documents[0].name).toBe("document1");
+})
+
+
+
+
